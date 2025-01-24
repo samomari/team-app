dfe4881fe8f8d9789f28438c0a11870cdf5aba74
MarSam <marius.sam98@gmail.com>
2025-01-24 03:13:33 +0200

    Setup db

---
+++ drizzle/schema.ts
@@ -0,0 +1,13 @@
+import { pgTable, text, timestamp, uniqueIndex, uuid, varchar } from "drizzle-orm/pg-core";
+
+export const user = pgTable('users', {
+    id: uuid('id').primaryKey().defaultRandom(),
+    name: varchar('name',{ length: 255 }).notNull(),
+    email: varchar({ length: 255 }).notNull(),
+    password: text('password').notNull(),
+    status: varchar('status',{ length: 20 }).default('active'),
+    lastLogin: timestamp('last_seen').notNull().defaultNow(),
+    createdAt: timestamp('created_at').notNull().defaultNow(),
+}, (t) => [
+    uniqueIndex('email_idx').on(t.email),
+]);
